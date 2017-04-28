7281a2ee2d1febf1327e56ba98feab24ece3a117
Eskil J <eskil@vuzii.com>
2017-04-28 14:24:27 +0200

    more fuse - done - 46.2 seconds

--- MD.c
+++ MD.c
@@ -21,13 +21,7 @@
 #include <math.h>
 #include "coord.h"
 
-void visc_force(int N, double *f, double *visc, double *vel);
-
-void add_norm(int N, double *r, double *delta);
-
 double force(double W, double delta, double r);
-
-void wind_force(int N, double *f, double *visc, double vel);
 
 #define size 1.0
 
